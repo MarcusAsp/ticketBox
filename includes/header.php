6ff958d56ffa564ca4e944691eaef04efb2358a8
MarcusAsp <mackan.a.b@hotmail.com>
2019-04-01 12:09:51 +0200

    stucture and files

--- includes/header.php
+++ includes/header.php
@@ -15,34 +15,70 @@ session_start();
     <link rel="stylesheet" type="text/css" media="screen" href="assets/css/style.css">
     <script src="assets/functions/jquery.slim.min.js"></script>
     <script src="assets/functions/bootstrap.min.js"></script>
+    <script src="assets/functions/functions.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
 </head>
 
 <body>
-<main>
-    <!-- HEADER -->
-<header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="../public/index.php"><img src="../public/assets/images/logos/sitelogo.png"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="../public/index.php">Home<span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Concerts</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Upcoming events</a>
-                </li>
-            </ul>
-            <span class="navbar-text">
-            <button type="button" class="btn btn-outline-secondary text-dark">Log in</button>
-            </span>
-        </div>
-    </nav>
-</header> 
+    <main>
+        <!-- HEADER -->
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="../public/index.php"><img src="../public/assets/images/logos/sitelogo.png"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarText">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="../public/index.php">Home<span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Concerts</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Upcoming events</a>
+                        </li>
+                    </ul>
+                    <span class="navbar-text">
+
+                        <button type="button" class="btn btn-outline-secondary text-dark" data-toggle="modal" data-target="#exampleModal" id="logIn">Log in</button>
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Log In</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form>
+                                            <div class="form-group">
+                                                <label for="exampleInputEmail1">Email address</label>
+                                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="exampleInputPassword1">Password</label>
+                                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                                            </div>
+                                            <div class="form-check">
+                                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Sign up</button>
+                                        <button type="button" class="btn btn-primary">Log In</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </span>
+                </div>
+            </nav>
+        </header> 
